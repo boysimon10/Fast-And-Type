abec680fb6eec26d071e59aa9703fcc98ec2d2e2
boysimon10 <simondiouf2710@gmail.com>
2023-09-20 16:36:21 +0000

    V1.2 script js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ const randomIndex = jeu.getRandomInt(listWords.length);
 const randomWord = listWords[randomIndex];
 
 jeu.changeMot(randomWord);
+
+function surveillerSaisie() {
+    const inputElement = document.getElementById("type-mot");
+
+    inputElement.addEventListener("input", function() {
+        const valeurInput = inputElement.value
+        return valeurInput
+    });
+}
+surveillerSaisie()

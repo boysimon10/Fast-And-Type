33799191511bc644f46a2ee53ff7f29f9fe7d10d
Abdoulaye SARRE <123773943+asarre01@users.noreply.github.com>
2023-09-20 16:24:51 +0000

    V0.1.2 back

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,13 @@
 
 export function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
 }
 
 export function getWord(listWords){
-    const randomIndex = getRandomInt(listWords.length);
-    return listWords[randomIndex];
+    const randomIndex = getRandomInt(listWords.length)
+    return listWords[randomIndex]
+}
+export function changeMot(m) {
+    const motElement = document.getElementById("mot")
+    motElement.textContent = m
 }
--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 import * as jeu from './functions.js';
 
 const listWords = ["apple","banana","cherry"];
+const randomIndex = jeu.getRandomInt(listWords.length);
+const randomWord = listWords[randomIndex];
 
-const motElement = document.getElementById("mot");
-function changeMot() {
-    const randomIndex = jeu.getRandomInt(listWords.length);
-    const randomWord = listWords[randomIndex];
-    motElement.textContent = randomWord;
-}
-changeMot();
+jeu.changeMot(randomWord);

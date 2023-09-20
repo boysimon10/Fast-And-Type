8f4e5f77ebf85bd2aa67ed1667f09f6fbecea58b
boysimon10 <simondiouf2710@gmail.com>
2023-09-20 17:22:43 +0000

    Version ça marche sans score et timer

--- js/script.js
+++ js/script.js
@@ -6,12 +6,42 @@ const randomWord = listWords[randomIndex];
 
 jeu.changeMot(randomWord);
 
-function surveillerSaisie() {
-    const inputElement = document.getElementById("type-mot");
 
-    inputElement.addEventListener("input", function() {
-        const valeurInput = inputElement.value
-        return valeurInput
-    });
-}
-surveillerSaisie()
+const inputElement = document.getElementById("type-mot");
+
+inputElement.addEventListener("input", function() {
+
+    const valeurInput = inputElement.value.toLowerCase()
+    const element = document.getElementById("type-mot")
+
+    let erreur = false
+    for (let i = 0; i < valeurInput.length; i++) {
+        if (valeurInput[i] !== randomWord[i]) {
+            erreur = true;
+            break;
+        }
+    }
+        if (erreur) {
+
+            if (element.classList.contains("focus:outline-none")) {
+                element.classList.remove("focus:outline-none")
+            }else if (element.classList.contains("focus:outline-green-700")) {
+                element.classList.remove("focus:outline-green-700")
+            }else{
+                element.classList.add("focus:outline-red-700")
+            }
+
+        } else if (valeurInput.length < randomWord.length) {
+
+            if (element.classList.contains("focus:outline-none")) {
+                element.classList.remove("focus:outline-none")
+            }else if (element.classList.contains("focus:outline-red-700")) {
+                element.classList.remove("focus:outline-red-700")
+            }else{
+                element.classList.add("focus:outline-green-700")
+            }
+
+        } else if (valeurInput.length === randomWord.length) {
+            
+        }
+});

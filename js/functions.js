73956e37a138fed77b38c4eb959683109d10a263
Abdoulaye SARRE <123773943+asarre01@users.noreply.github.com>
2023-09-23 02:00:29 +0000

    V0.2 Mise à jour de l'input

--- js/functions.js
+++ js/functions.js
@@ -7,17 +7,7 @@ export function getWord(listWords){
     const randomIndex = getRandomInt(listWords.length)
     return listWords[randomIndex]
 }
-export function changeMot(m) {
+export function changeWord(m) {
     const motElement = document.getElementById("mot")
     motElement.textContent = m
-}
-
-export function divMot(mot) {
-    const tableauLettres = [];
-
-    for (let i = 0; i < mot.length; i++) {
-        tableauLettres.push(mot[i]);
-    }
-
-    return tableauLettres;
 }
--- js/script.js
+++ js/script.js
@@ -4,44 +4,32 @@ const listWords = ["apple","banana","cherry"];
 const randomIndex = jeu.getRandomInt(listWords.length);
 const randomWord = listWords[randomIndex];
 
-jeu.changeMot(randomWord);
+jeu.changeWord(randomWord);
 
 
 const inputElement = document.getElementById("type-mot");
 
 inputElement.addEventListener("input", function() {
+    const valeurInput = inputElement.value.toLowerCase();
+    const errorClass = "border-red-700";
+    const successClass = "border-green-700";
+    const defaultClass = "border-blue-700";
 
-    const valeurInput = inputElement.value.toLowerCase()
-    const element = document.getElementById("type-mot")
+    let hasError = false;
+    let hasSuccess = false;
 
-    let erreur = false
     for (let i = 0; i < valeurInput.length; i++) {
         if (valeurInput[i] !== randomWord[i]) {
-            erreur = true;
+            hasError = true;
             break;
         }
     }
-        if (erreur) {
 
-            if (element.classList.contains("focus:outline-none")) {
-                element.classList.remove("focus:outline-none")
-            }else if (element.classList.contains("focus:outline-green-700")) {
-                element.classList.remove("focus:outline-green-700")
-            }else{
-                element.classList.add("focus:outline-red-700")
-            }
+    if (valeurInput.length <= randomWord.length && !hasError) {
+        hasSuccess = true;
+    }
 
-        } else if (valeurInput.length < randomWord.length) {
-
-            if (element.classList.contains("focus:outline-none")) {
-                element.classList.remove("focus:outline-none")
-            }else if (element.classList.contains("focus:outline-red-700")) {
-                element.classList.remove("focus:outline-red-700")
-            }else{
-                element.classList.add("focus:outline-green-700")
-            }
-
-        } else if (valeurInput.length === randomWord.length) {
-            
-        }
+    inputElement.classList.toggle(errorClass, hasError);
+    inputElement.classList.toggle(successClass, hasSuccess);
+    inputElement.classList.toggle(defaultClass, !hasError && !hasSuccess);
 });
